f5f9822c4401a8f6c203477f7139c71709cf5040
Oleg <olazar61@gmail.com>
2024-04-28 22:04:31 +0300

    Getting to know Flask

---
+++ l_1_getting_to_know_flask/app_08.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hi!'
+
+
+@app.route('/if/')
+def show_if():
+    context = {
+            'title': 'Ветвление',
+            'user': 'Хакер',
+            'number': 6
+            }
+    return render_template('show_if.html', **context)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from l_1_getting_to_know_flask.app_01 import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
